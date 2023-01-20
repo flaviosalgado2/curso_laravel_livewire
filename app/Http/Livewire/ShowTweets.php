0dfd91645f7af99e6739c6c64890e55197d4fcbd
flaviosalgado2 <flaviosalgado2@hotmail.com>
2023-01-19 22:30:10 -0300

    populando usuario laravel em tabela, seed, factory, brincando com blade.

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Tweet;
 use Livewire\Component;
 
 class ShowTweets extends Component
 {
+
+    public $message = 'Apenas um teste 2';
+
     public function render()
     {
-        return view('livewire.show-tweets');
+        $tweets = Tweet::with('user')->get();
+
+        // dd($tweets);
+
+        return view('livewire.show-tweets', [
+            'tweets' => $tweets
+        ]);
     }
 }
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,3 +1,13 @@
 <div>
     Show Tweets - Curso Laravel Livewire - Flávio Salgado
+    <br>
+    {{ $message }}
+    <br>
+    <input type="text" name="message" id="message" wire:model="message">
+
+    <hr>
+
+    @foreach ($tweets as $tweet)
+        {{ $tweet->user->name }} - {{ $tweet->content }} <br>
+    @endforeach
 </div>
